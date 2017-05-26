838df738b589ec8157236bc7f4465f616c122b2c
Diego Sosa <diego.venezuela@gmail.com>
2017-05-26 11:48:24 +0200

    Getting external libraries for assertions

---
+++ lesson-01/simplestTest.go
@@ -0,0 +1,9 @@
+    package main 
+
+    import ( "testing"
+             "github.com/stretchr/testify/assert" 
+    )
+
+    func testSimplest() {
+        assert.Equal(123, 123, "they should be equal")
+    }   
