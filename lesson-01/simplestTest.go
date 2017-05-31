696b398a647232f9ea15575e71f12bee15bb68bb
Diego Sosa <diego.venezuela@gmail.com>
2017-06-01 01:05:49 +0200

    Named fixed

--- lesson-01/simplestTest.go
+++
@@ -1,9 +0,0 @@
-    package main 
-
-    import ( "testing"
-             "github.com/stretchr/testify/assert" 
-    )
-
-    func testSimplest() {
-        assert.Equal(123, 123, "they should be equal")
-    }   
